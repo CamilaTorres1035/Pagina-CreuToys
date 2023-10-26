1cbde422d1a67eb9e961435870b6aa28f1ae71f2
Camss <106281035+CamilaTorres1035@users.noreply.github.com>
2023-10-25 20:46:50 -0500

    70%, no funcional al 100%

---
+++ database.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-10-2023 a las 01:17:16
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `creutoys`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `codped` int(11) NOT NULL,
+  `codusu` int(11) NOT NULL,
+  `codpro` int(11) NOT NULL,
+  `fecped` datetime NOT NULL,
+  `estado` int(11) NOT NULL,
+  `dirusuped` varchar(50) NOT NULL,
+  `telusuped` varchar(12) NOT NULL,
+  `token` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `codpro` int(11) NOT NULL,
+  `nompro` varchar(50) DEFAULT NULL,
+  `despro` varchar(100) DEFAULT NULL,
+  `prepro` decimal(6,2) DEFAULT NULL,
+  `estado` int(11) DEFAULT NULL,
+  `rutimapro` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`codpro`, `nompro`, `despro`, `prepro`, `estado`, `rutimapro`) VALUES
+(1, 'Animales', 'Arbol de juegos con animales', '9.99', 1, 'Animales.png'),
+(2, 'Baby Yoda', 'Muñeco de Baby Yoda | Star Wars', '9.99', 1, 'Baby-Yoda.png'),
+(3, 'Stunt Drone', 'Drone con contol remoto', '9.99', 1, 'Drone.png'),
+(4, 'Muro de Escalar', 'Muro para escalar 5 en 1', '9.99', 1, 'Escalar.png'),
+(5, 'Furby', 'Muñeco Furby parlante', '9.99', 1, 'Furby.png'),
+(6, 'Stitch', 'Peluche de Stitch suave y abrazable', '9.99', 1, 'Stitch.jpg'),
+(7, 'Tren Thomas', 'Tren parlante de Thomas & friends', '9.99', 1, 'Thomas.png'),
+(8, 'Zona de Deporte', 'Zona de juegos de deportes', '9.99', 1, 'Zona-Deporte.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `codusu` int(11) NOT NULL,
+  `nomusu` varchar(50) DEFAULT NULL,
+  `apeusu` varchar(50) DEFAULT NULL,
+  `emausu` varchar(50) NOT NULL,
+  `pasusu` varchar(20) NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`codusu`, `nomusu`, `apeusu`, `emausu`, `pasusu`, `estado`) VALUES
+(1, 'Usuario', 'Demo', 'correo@example.com', '123456', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`codped`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`codpro`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`codusu`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `codped` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `codpro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `codusu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
